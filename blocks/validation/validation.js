998803f789d91340b1f0f596967d7de9b8fb1ea3
Dragos Cristian Bute <dbute@dragoss-mbp.eur.adobe.com>
2023-02-23 13:59:13 +0200

    Working api

---
+++ blocks/validation/validation.js
@@ -0,0 +1,5 @@
+export default class Validation {
+    constructor(text) {
+        this.text = text;
+    }
+}
--- blocks/validations/validations.js
+++ blocks/validations/validations.js
@@ -1,15 +1,39 @@
-export default function decorate(block) {
-    let response = getResponse();
+import Validation from "../validation/validation.js";
 
-    console.log(response);
+export default function decorate(block) {
+   const validations = getValidations(block);
+
 }
 
 
 
-const getResponse = async () => {
-    const response = await fetch('http://localhost:8080/api/validations');
-    let myJson = await response.json(); //extract JSON from the http response
-    // do something with myJson
+const response = fetch('http://localhost:8080/api/validations')
+    .then((response) => response.json())
+    .then((validation) => {
+        return validation;
+    });
 
-    return myJson;
-  }
+
+
+const getValidations = (block) => {
+    const list = document.createElement("ul");
+    let validations=[];
+    response.then((validationResponse) => {
+        validationResponse['_embedded']["validations"].forEach(element => {
+            console.log(element)
+            validations.push(element);
+            let child = document.createElement("li");
+            child.innerHTML = "Validation id:" + element["validationId"] + " | " + "programId:" + element["programId"] + " | " + "environmentId:" + element["environmentId"] + " | " + "status:" + element["status"];
+
+            list.appendChild(child)
+
+            console.log(child);
+        });
+
+        
+    }).then(() => {
+        block.appendChild(list)
+    })
+    
+    return validations;
+}
